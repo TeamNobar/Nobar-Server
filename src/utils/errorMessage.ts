e222dfb4d2fef9f048462ba12e3dc42146e8b66d
= <tmdgk490255@gmail.com>
2022-07-12 20:24:25 +0900

    feat(chore): errorMessage로 파일 이름 변경
    responseMessage에서 errorMessage로 이름 변경

---
+++ src/utils/errorMessage.ts
@@ -0,0 +1,7 @@
+export const errorMessage = {
+  NULL_VALUE: "필요한 값이 없습니다.",
+  NOT_FOUND: "존재하지 않는 자원",
+  BAD_REQUEST: "잘못된 요청",
+  INTERNAL_SERVER_ERROR: "서버 내부 오류",
+  VALIDATION_FAILED: "처리 할 수 없는 값 입니다."
+}
