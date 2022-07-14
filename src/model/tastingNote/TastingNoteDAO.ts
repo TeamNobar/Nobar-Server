a1712df3660216bf1c4c415fb2806442dc1e6613
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-15 00:09:25 +0900

    feat(model): create TastingNoteDAO

---
+++ src/model/tastingNote/TastingNoteDAO.ts
@@ -0,0 +1,34 @@
+import mongoose    from "mongoose";
+import TastingNote from "./TastingNote";
+
+const tastingNoteSchema = new mongoose.Schema({
+  rate: {
+    type: Number,
+    required: true
+  },
+  recipe: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Recipe"
+  },
+  tastingTags: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "NoteSeletedTag"
+  },
+  tasteContent: {
+    type: String,
+    defualt: ""
+  },
+  experienceContent: {
+    type: String,
+    default: ""
+  }
+}, {
+  timestamps: {
+    createdAt: "CreatedAt",
+    currentTime: () => Math.floor(Date.now() / 1000)
+  }
+});
+
+export default mongoose.model<mongoose.Document & TastingNote>("TastingNote", tastingNoteSchema);
