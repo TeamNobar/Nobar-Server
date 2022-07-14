fce304e9fcf7dfb66b3bd59562db94d2621ddc05
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-15 00:08:41 +0900

    feat(model): create tastingNote Entity

---
+++ src/model/tastingNote/TastingNote.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export default interface TastingNote {
+  rate: number,
+  recipe: mongoose.Schema.Types.ObjectId
+  tastingTag: mongoose.Schema.Types.ObjectId[],
+  tasteContent: string,
+  experienceContent: string,
+  createAt: number
+}
