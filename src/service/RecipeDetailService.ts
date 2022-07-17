f0733db236bcfb06d05b29a95f1ce8ae72b8f253
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-17 18:29:00 +0900

    feat(recipe): Add ingredientDAO in RecipeDetailService

--- src/service/RecipeDetailService.ts
+++ src/service/RecipeDetailService.ts
@@ -1,9 +1,11 @@
-import { Model }  from "mongoose";
-import { Recipe } from "../model/recipe/Recipe";
+import { Model }      from "mongoose";
+import { Ingredient } from "../model/ingredient/Ingredient";
+import { Recipe }     from "../model/recipe/Recipe";
 
 export default class RecipeDetailService {
   constructor(
-    private readonly recipeDAO: Model<Recipe & Document>
+    private readonly recipeDAO: Model<Recipe & Document>,
+    private readonly ingredient: Model<Ingredient & Document>
   ) {
   }
 };
