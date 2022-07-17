ca6b45addc8accd8508848a1bbf209887695886f
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-18 01:12:02 +0900

    feat(recipe): find Recipe Detail by id function

--- src/service/RecipeDetailService.ts
+++ src/service/RecipeDetailService.ts
@@ -1,11 +1,21 @@
-import { Model }      from "mongoose";
-import { Ingredient } from "../model/ingredient/Ingredient";
-import { Recipe }     from "../model/recipe/Recipe";
+import { Document, Model } from "mongoose";
+import logger              from "../loaders/Logger";
+import { Recipe }          from "../model/recipe/Recipe";
 
 export default class RecipeDetailService {
   constructor(
     private readonly recipeDAO: Model<Recipe & Document>,
-    private readonly ingredient: Model<Ingredient & Document>
   ) {
+  }
+
+  public async getRecipeDetail(recipeId: string) {
+    try {
+      const recipe = await this.recipeDAO.findById(recipeId)
+        .populate("base");
+      return recipe;
+    } catch (error) {
+      logger.error(error);
+      throw error
+    }
   }
 };
