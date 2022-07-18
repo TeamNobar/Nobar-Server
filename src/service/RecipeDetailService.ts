78c8de7d1469514aee6e03a3790ce643ef6e9510
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-18 22:36:24 +0900

    feat(recipe): 레시피 디테일 정보 조회 함수 분리

--- src/service/RecipeDetailService.ts
+++ src/service/RecipeDetailService.ts
@@ -1,13 +1,13 @@
 import { Document, Model } from "mongoose";
+import { RecipeDetailDTO } from "../dto/RecipeDetailDTO";
 import NobarError          from "../error/NobarError";
 import { NobarErrorCode }  from "../error/NobarErrorCode";
 import NobarErrorMessage   from "../error/NobarErrorMessage";
 import logger              from "../loaders/Logger";
 import { Base }            from "../model/base/Base";
-import BaseDAO             from "../model/base/BaseDAO";
 import BaseEntity          from "../model/base/entity/BaseEntity";
 import { Ingredient }      from "../model/ingredient/Ingredient";
-import IngredientDAO       from "../model/ingredient/IngredientDAO";
+import IngredientEntity    from "../model/ingredient/IngredientEntity";
 import RecipeEntity        from "../model/recipe/entity/RecipeEntity";
 import { Recipe }          from "../model/recipe/Recipe";
 
@@ -25,15 +25,12 @@ export default class RecipeDetailService {
       /*const recipe: RecipeEntity | null = await this.recipeDAO.findById(recipeId)
         .populate({path: "base", model: BaseDAO})
         .populate({path: "ingredients.ingredient", model: IngredientDAO});*/
-      const recipe: RecipeEntity | null = await this.recipeDAO.findById(recipeId);
-      if (!recipe) {
-        throw new NobarError(NobarErrorCode.BAD_REQUST, NobarErrorMessage.NOT_FOUND_RECIPE);
-      }
-
-      const base: BaseEntity | null
-      const ingredients = recipe.ingredients.map(value => {
-        value.ingredient.valueOf();
-      })
+      const recipe: RecipeEntity = await this.findRecipeById(recipeId);
+      const base: BaseEntity = await this.findBaseById(recipe.base.valueOf().toString())
+      const ingredients: IngredientEntity[] = Promise.all(recipe.ingredients.map(async (value) => {
+        await this.findIngredientById(value.ingredient.valueOf().toString());
+      }));
+      const RecipeDetailResponse: RecipeDetailDTO =
       return {
         id: recipe._id.valueOf().toString(),
       };
@@ -43,9 +40,26 @@ export default class RecipeDetailService {
     }
   }
 
-  private validateReicpe(recipe: RecipeEntity | null) {
-    throw new NobarError(
-      NobarErrorCode.BAD_REQUST
-    )
+  private async findRecipeById(recipeId: string): Promise<RecipeEntity> {
+    const recipe: RecipeEntity | null = await this.recipeDAO.findById(recipeId);
+    if (!recipe) {
+      throw new NobarError(NobarErrorCode.BAD_REQUST, NobarErrorMessage.NOT_FOUND_RECIPE);
+    }
+    return recipe;
+  }
+
+  private async findBaseById(baseId: string): Promise<BaseEntity> {
+    const base: BaseEntity | null = await this.baseDAO.findById(baseId);
+    if (!base) {
+      throw new NobarError(NobarErrorCode.BAD_REQUST, NobarErrorMessage.NOT_FOUND_BASE);
+    }
+    return base;
+  }
+
+  private async findIngredientById(ingredientId: string) {
+    const ingredient: IngredientEntity | null = await this.ingredientDAO.findById(ingredientId);
+    if (!ingredient) {
+      throw new NobarError(NobarErrorCode.BAD_REQUST, NobarErrorMessage.NOT_FOUND_BASE);
+    }
   }
 }
