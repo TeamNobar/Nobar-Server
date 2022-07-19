45c2de6b0b211b37928d6312ab1d261b61138bb2
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-19 13:01:33 +0900

    fix: Fix RecipeDetailDTO base field type to BaseDTO

--- src/dto/RecipeDetailDTO.ts
+++ src/dto/RecipeDetailDTO.ts
@@ -1,11 +1,12 @@
 import RecipeIngredient from "../model/recipe/RecipeIngredient";
+import { BaseDTO }      from "./base/BaseDTO";
 
 export interface RecipeDetailDTO {
   id: string
   name: string,
   enName: string,
   version: string[],
-  base: string,
+  base: BaseDTO,
   proof: number,
   proofIcon: string,
   skill: number,
