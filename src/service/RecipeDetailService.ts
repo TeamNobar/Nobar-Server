b8b4b1d50da7957d6048b7ba10432e2657a50673
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-20 03:08:17 +0900

    feat: service retrun DTO

--- src/service/RecipeDetailService.ts
+++ src/service/RecipeDetailService.ts
@@ -1,16 +1,18 @@
-import mongoose, { Model } from "mongoose";
-import { type }     from "os";
-import RecipeMapper from "src/model/recipe/mapper/RecipeMapper";
-import NobarError   from "../error/NobarError";
-import { NobarErrorCode } from "../error/NobarErrorCode";
-import NobarErrorMessage from "../error/NobarErrorMessage";
-import logger from "../loaders/Logger";
-import { Base } from "../model/base/Base";
-import BaseEntity from "../model/base/entity/BaseEntity";
-import { Ingredient } from "../model/ingredient/Ingredient";
-import IngredientEntity from "../model/ingredient/IngredientEntity";
-import RecipeEntity from "../model/recipe/entity/RecipeEntity";
-import { Recipe } from "../model/recipe/Recipe";
+import mongoose, { Model }   from "mongoose";
+import { RecipeDetailDTO }   from "../dto/recipe/RecipeDetailDTO";
+import NobarError            from "../error/NobarError";
+import { NobarErrorCode }    from "../error/NobarErrorCode";
+import NobarErrorMessage     from "../error/NobarErrorMessage";
+import logger                from "../loaders/Logger";
+import { Base }              from "../model/base/Base";
+import BaseEntity            from "../model/base/entity/BaseEntity";
+import { Ingredient }        from "../model/ingredient/Ingredient";
+import IngredientEntity      from "../model/ingredient/IngredientEntity";
+import RecipeEntity          from "../model/recipe/entity/RecipeEntity";
+import RecipeIngredientEmbed from "../model/recipe/mapper/RecipeImgredientEmbed";
+import RecipeMapper          from "../model/recipe/mapper/RecipeMapper";
+import { Recipe }            from "../model/recipe/Recipe";
+import RecipeIngredient      from "../model/recipe/RecipeIngredient";
 
 export default class RecipeDetailService {
   constructor(
@@ -20,7 +22,7 @@ export default class RecipeDetailService {
   ) {
   }
 
-  public async getRecipeDetail(recipeId: string) {
+  public async getRecipeDetail(recipeId: string): Promise<RecipeDetailDTO> {
     try {
       // DI 하고 싶어서 분리하고 싶은데 내부에서 DAO 그대로 하면 무슨 소용
       /*const recipe: RecipeEntity | null = await this.recipeDAO.findById(recipeId)
@@ -29,10 +31,9 @@ export default class RecipeDetailService {
       const recipe: RecipeEntity = await this.findRecipeById(recipeId);
       const base: BaseEntity = await this.findBaseById(recipe.base.valueOf().toString());
       const recipeVersions: RecipeEntity[] | null = await this.findRecipeVersionNames(recipeId);
-      RecipeMapper.toRecipeDetailDTO(recipe, base, recipeVersions)
-      return {
-        id: versionNames
-      };
+      const embededIngredient: RecipeIngredientEmbed[] = await this.embedAllIngredient(recipe.ingredients);
+      const recipeDetailDTO: RecipeDetailDTO = RecipeMapper.toRecipeDetailDTO(recipe, base, recipeVersions, embededIngredient);
+      return recipeDetailDTO
     } catch (error) {
       logger.error(error);
       throw error
@@ -59,10 +60,26 @@ export default class RecipeDetailService {
     return this.recipeDAO.find({defaultRecipe: defaultRecipeId});
   }
 
-  private async findIngredientById(ingredientId: string) {
+  private async embedAllIngredient(ingredients: RecipeIngredient[]): Promise<RecipeIngredientEmbed[]> {
+    return Promise.all(
+      ingredients.map(async value => await this.embedIngredient(value))
+    )
+  }
+
+  private async embedIngredient(recipeIngredient: RecipeIngredient) {
+    const ingredient: IngredientEntity = await this.findIngredientById(recipeIngredient.ingredient.valueOf().toString());
+    return <RecipeIngredientEmbed>{
+      ingredient: ingredient,
+      quantity: recipeIngredient.quantity,
+      unit: recipeIngredient.unit
+    }
+  }
+
+  private async findIngredientById(ingredientId: string): Promise<IngredientEntity> {
     const ingredient: IngredientEntity | null = await this.ingredientDAO.findById(ingredientId);
     if (!ingredient) {
       throw new NobarError(NobarErrorCode.BAD_REQUEST, NobarErrorMessage.NOT_FOUND_BASE);
     }
+    return ingredient;
   }
 }
