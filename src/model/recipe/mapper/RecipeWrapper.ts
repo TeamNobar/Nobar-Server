936e1f2efc2417768342bd66e4124ee7972e9847
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-19 12:48:33 +0900

    feat(recipe): RecipeDetailDTO 변환 함수 인자로 base, recipeVersion 추가

--- src/model/recipe/mapper/RecipeWrapper.ts
+++ src/model/recipe/mapper/RecipeWrapper.ts
@@ -5,9 +5,21 @@ import RecipeEntity        from "../entity/RecipeEntity";
 
 export default class RecipeWrapper {
   public static toRecipeDetailDTO(
-    recipeEntity: RecipeEntity,
-    baseEntity: BaseEntity
+    recipe: RecipeEntity,
+    base: BaseEntity,
+    recipeVersions: RecipeEntity[] | null
   ): RecipeDetailDTO {
+    return <RecipeDetailDTO>{
+      id: recipe._id,
+      name: recipe.name,
+      enName: recipe.enName,
+      defaultRecipe
+    }
+  }
 
+  private getRecipeNames(recipeVersions: RecipeEntity[] | null) {
+    return recipeVersions?.map(
+      value => value.version
+    );
   }
 };
