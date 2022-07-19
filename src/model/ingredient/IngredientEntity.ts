d331a4ed369c5ee51ef0a22309bd1efac97429df
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-20 02:58:34 +0900

    feat: fix IngredientEntity has _id field

--- src/model/ingredient/IngredientEntity.ts
+++ src/model/ingredient/IngredientEntity.ts
@@ -1,4 +1,6 @@
+import mongoose       from "mongoose";
 import { Ingredient } from "./Ingredient";
 
 export default interface IngredientEntity extends Ingredient {
+  _id: mongoose.Schema.Types.ObjectId,
 }
