0b2d899deebab783f6174bab3bcaf8ac648cbb4e
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-19 12:45:55 +0900

    feat: Add RecipeDetailService dependency
    - baseDAO 에 대한 의존성을 외부에서 주입하도록 작성
    - RecipeVersions 에 대한 의존성을 외부에서 주입하도록 작성

--- src/service/ServiceInjector.ts
+++ src/service/ServiceInjector.ts
@@ -1,10 +1,14 @@
+import BaseDAO             from "../model/base/BaseDAO";
+import IngredientDAO       from "../model/ingredient/IngredientDAO";
 import RecipeDAO           from "../model/recipe/RecipeDAO";
 import RecipeDetailService from "./RecipeDetailService";
 
 export default class ServiceInjector {
   public static get recipeDetail() {
     return new RecipeDetailService(
-      RecipeDAO
+      RecipeDAO,
+      BaseDAO,
+      IngredientDAO
     )
   }
-};
+}
