76cbe2361c6e0aff8b008d617a41e1f9ac9b1315
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-19 13:06:15 +0900

    feat: RecipeWrapper to RecipeMapper

--- src/model/recipe/mapper/RecipeMapper.ts
+++ src/model/recipe/mapper/RecipeMapper.ts
@@ -1,25 +1,31 @@
 import { RecipeDetailDTO } from "../../../dto/RecipeDetailDTO";
 import BaseEntity          from "../../base/entity/BaseEntity";
+import BaseMapper          from "../../base/mapper/BaseMapper";
 import IngredientEntity    from "../../ingredient/IngredientEntity";
 import RecipeEntity        from "../entity/RecipeEntity";
 
-export default class RecipeWrapper {
+export default class RecipeMapper {
   public static toRecipeDetailDTO(
     recipe: RecipeEntity,
     base: BaseEntity,
     recipeVersions: RecipeEntity[] | null
   ): RecipeDetailDTO {
     return <RecipeDetailDTO>{
-      id: recipe._id,
+      id: recipe._id.valueOf().toString(),
       name: recipe.name,
       enName: recipe.enName,
-      defaultRecipe
+      version: this.getRecipeNames(recipeVersions),
+      base: BaseMapper.toBaseDTO(base),
+      proof: recipe.proof,
+      proofIcon: recipe.proofIcon,
+      skill: recipe.skill,
+      glass: recipe.glass
     }
   }
 
-  private getRecipeNames(recipeVersions: RecipeEntity[] | null) {
+  private static getRecipeNames(recipeVersions: RecipeEntity[] | null): string[] {
     return recipeVersions?.map(
       value => value.version
-    );
+    ) || []
   }
 };
