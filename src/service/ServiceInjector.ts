fd7a10d6f39df5bb5a086d6cbfe157ef6a4433b3
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-22 03:02:35 +0900

    feat(tasting): Add tastingNoteService injector

--- src/service/ServiceInjector.ts
+++ src/service/ServiceInjector.ts
@@ -1,9 +1,11 @@
 import BaseDAO             from "../model/base/BaseDAO";
 import IngredientDAO       from "../model/ingredient/IngredientDAO";
 import RecipeDAO           from "../model/recipe/RecipeDAO";
+import TastingNoteDAO      from "../model/tastingNote/TastingNoteDAO";
 import UserDAO             from "../model/user/UserDAO";
 import AuthService         from "./AuthService";
 import RecipeDetailService from "./RecipeDetailService";
+import TastingNoteService  from "./TastingNoteService";
 
 export default class ServiceInjector {
   public static get recipeDetail() {
@@ -17,6 +19,12 @@ export default class ServiceInjector {
   public static get auth() {
     return new AuthService(
       UserDAO
+    )
+  }
+
+  public static get tastingNote() {
+    return new TastingNoteService(
+      TastingNoteDAO
     )
   }
 }
