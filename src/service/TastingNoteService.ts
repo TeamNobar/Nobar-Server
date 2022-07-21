903b682d3c55942b47e980cd0bb979a8363ac3da
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-22 03:03:27 +0900

    feat(tasting): Add TastingNoteService constructor

--- src/service/TastingNoteService.ts
+++ src/service/TastingNoteService.ts
@@ -1,3 +1,12 @@
+import { Document, Model } from "mongoose";
+import TastingTagMapper    from "../model/tastingNote/mapper/TastingTagMapper";
+import TastingNote         from "../model/tastingNote/TastingNote";
+import { TastingNoteTag }  from "../model/tastingNote/TastingNoteTag";
+
 export default class TastingNoteService {
+  constructor(
+    private readonly tastingNoteDAO: Model<TastingNote & Document>
+  ) {
+  }
 
 };
