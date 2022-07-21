53699007e5236fbd3b7d39649fc6a62479bb0631
jinsu4755 <parkjinsu4755@gmail.com>
2022-07-22 02:06:52 +0900

    feat(auth): token 읽는 로직 tsoa 공식문서 로직 사용

--- src/auth/expressAuthentication.ts
+++ src/auth/expressAuthentication.ts
@@ -1,29 +1,56 @@
-import express            from "express";
-import jwt                from "jsonwebtoken"
-import config             from "../config";
-import NobarError         from "../error/NobarError";
-import { NobarErrorCode } from "../error/NobarErrorCode";
-import NobarErrorMessage  from "../error/NobarErrorMessage";
+import express from "express";
+import jwt     from "jsonwebtoken"
+import config  from "../config";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 export function expressAuthentication(
   request: express.Request,
-  securityName: string
-) {
-  if (securityName === "auth0") {
-    const token = request.headers["authorization"]?.split(" ").reverse()[0];
-    return new Promise(((resolve, reject) => {
+  securityName: string,
+  _scopes?: string[]
+): Promise<any> {
+  if (securityName === "api_key") {
+    let token;
+    if (request.query && request.query.access_token) {
+      token = request.query.access_token;
+    }
+
+    if (token === "abc123456") {
+      return Promise.resolve({
+        id: 1,
+        name: "Ironman",
+      });
+    } else {
+      return Promise.reject({});
+    }
+  }
+
+  if (securityName === "jwt") {
+    const token =
+      request.body.token ||
+      request.query.token ||
+      request.headers["token"];
+
+    return new Promise((resolve, reject) => {
       if (!token) {
-        throw new NobarError(NobarErrorCode.NOT_FOUND_TOKEN, NobarErrorMessage.NOT_FOUND_TOKEN);
+        reject(new Error("No token provided"));
       }
-      jwt.verify(token, config.jwtSecret, function (err: jwt.VerifyErrors | null, decoded: string | jwt.JwtPayload | undefined) {
+      jwt.verify(token, config.jwtSecret, function (err: any, decoded: any) {
         if (err) {
           reject(err);
         } else {
+          // Check if JWT contains all required scopes
+          /*if (!scopes) {
+            resolve(decoded);
+            return;
+          }
+          for (const scope of scopes) {
+            if (!decoded.scopes.includes(scope)) {
+              reject(new Error("JWT does not contain required scope."));
+            }
+          }*/
           resolve(decoded);
         }
       });
-    }))
+    });
   }
+  return new Promise((_, reject) => reject());
 }
