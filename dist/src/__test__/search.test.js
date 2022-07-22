a7a831295fcea9a7e3720b3a0573201c63fa9315
= <tmdgk490255@gmail.com>
2022-07-22 13:48:43 +0900

    feat(test): 테스트 코드 파일 하나로 합침
    테스트 코드 파일 하나로 합침

--- dist/src/__test__/search.test.js
+++ dist/src/__test__/search.test.js
@@ -14,8 +14,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const index_1 = require("../index");
+const mongoose_1 = __importDefault(require("mongoose"));
+const GuideDAO_1 = __importDefault(require("../model/guide/GuideDAO"));
+// import { NobarErrorCode } from "../error/NobarErrorCode";
 describe("search", () => {
-    // afterAll(() => DB.close());
+    afterAll(() => __awaiter(void 0, void 0, void 0, function* () {
+        console.log("서치 디스커넥션");
+        yield mongoose_1.default.connection.close();
+    }));
     describe("given the search tags exist", () => {
         it("should return a 200 status", () => __awaiter(void 0, void 0, void 0, function* () {
             const { statusCode } = yield (0, supertest_1.default)(index_1.app)
@@ -25,7 +31,7 @@ describe("search", () => {
     });
     describe("given the keyword is sent", () => {
         it("should return a 200 status", () => __awaiter(void 0, void 0, void 0, function* () {
-            const keyword = "레몬";
+            const keyword = encodeURI("레몬");
             const { statusCode } = yield (0, supertest_1.default)(index_1.app)
                 .get(`/search/keyword?keyword=${keyword}`);
             expect(statusCode).toBe(200);
@@ -33,7 +39,7 @@ describe("search", () => {
     });
     describe("given the base is sent", () => {
         it("should return a 200 status", () => __awaiter(void 0, void 0, void 0, function* () {
-            const base = "위스키";
+            const base = encodeURI("위스키");
             const { statusCode } = yield (0, supertest_1.default)(index_1.app)
                 .get(`/search/base?base=${base}`);
             expect(statusCode).toBe(200);
@@ -43,6 +49,33 @@ describe("search", () => {
         it("should return a 200 status", () => __awaiter(void 0, void 0, void 0, function* () {
             const { statusCode } = yield (0, supertest_1.default)(index_1.app)
                 .get("/search/tag");
+            expect(statusCode).toBe(200);
+        }));
+    });
+    describe("given the guide does not exist", () => {
+        it("should return a 400 status", () => __awaiter(void 0, void 0, void 0, function* () {
+            const guideId = "잘못된 ID";
+            const { statusCode } = yield (0, supertest_1.default)(index_1.app)
+                .get(`/guide/${guideId}`);
+            expect(statusCode).toBe(400);
+        }));
+    });
+    describe("given the guide does exist", () => {
+        it("should return a 200 status", () => __awaiter(void 0, void 0, void 0, function* () {
+            const newGuide = new GuideDAO_1.default({
+                title: "가이드제목수정",
+                subtitle: "가이드서브타이틀",
+                content: "가이드내용",
+                images: [
+                    "url1",
+                    "url2"
+                ],
+                thumbnail: "썸네일url3"
+            });
+            yield newGuide.save();
+            const newGuideId = newGuide._id;
+            const { statusCode } = yield (0, supertest_1.default)(index_1.app)
+                .get(`/guide/${newGuideId}`);
             expect(statusCode).toBe(200);
         }));
     });
--- src/__test__/search.test.ts
+++ src/__test__/search.test.ts
@@ -1,9 +1,14 @@
 import supertest from "supertest";
 import { app } from "../index";
+import mongoose from "mongoose";
+import GuideDAO from "../model/guide/GuideDAO";
+// import { NobarErrorCode } from "../error/NobarErrorCode";
 
 describe("search", () => {
 
-  // afterAll(() => DB.close());
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 
   describe("given the search tags exist", () => {
     it("should return a 200 status", async () => {
@@ -40,6 +45,40 @@ describe("search", () => {
 
       const { statusCode } = await supertest(app)
         .get("/search/tag");
+      expect(statusCode).toBe(200);    
+
+    })
+  })
+
+  
+  describe("given the guide does not exist", () => {
+    it("should return a 400 status", async () => {
+      const guideId = "잘못된 ID"; 
+      const { statusCode } = await supertest(app)
+        .get(`/guide/${guideId}`);
+      expect(statusCode).toBe(400);
+    })
+  });
+
+  describe("given the guide does exist", () => {
+    it("should return a 200 status", async () => {
+            
+      const newGuide = new GuideDAO({
+        title: "가이드제목수정",
+        subtitle: "가이드서브타이틀",
+        content: "가이드내용",
+        images: [
+          "url1",
+          "url2"
+        ],
+        thumbnail: "썸네일url3"
+      })
+
+      await newGuide.save();
+      const newGuideId = newGuide._id as unknown as string; 
+
+      const { statusCode } = await supertest(app)
+        .get(`/guide/${newGuideId}`);
       expect(statusCode).toBe(200);    
 
     })
